c632d5a8fc2b967d01d55dd7b367b6062d7f8953
Ramakrishna Medipally <ramakrishna.medipally@coxautoinc.com>
2018-02-01 16:01:22 +0530

    Added new repository file

--- src/main/java/com/dt/rts/ladmv/application/ServiceEndPoint.java
+++ src/main/java/com/dt/rts/ladmv/application/ServiceEndPoint.java
@@ -7,8 +7,11 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.dt.rts.ladmv.repository.DailyMoniesInquiryRepository;
 import com.dt.rts.ladmv.repository.TitleRegRepository;
 import com.dt.rts.ladmv.repository.VehicleInquiryRepository;
+import com.dt.rts.ladmv.services.inquiries.dailymoniessummaryinquiry.DailyMoniesSummaryInquiryRequest;
+import com.dt.rts.ladmv.services.inquiries.dailymoniessummaryinquiry.DailyMoniesSummaryInquiryResponse;
 import com.dt.rts.ladmv.services.inquiries.vehicleinquiry.VehicleInquiryRequest;
 import com.dt.rts.ladmv.services.inquiries.vehicleinquiry.VehicleInquiryResponse;
 import com.dt.rts.ladmv.services.transaction.titleregtransaction.TitleRegRequest;
@@ -19,27 +22,24 @@ import com.dt.rts.ladmv.services.transaction.titleregtransaction.TitleRegRespons
 public class ServiceEndPoint {
 	private static final String VM_NAMESPACE_URI = "http://rts.dt.com/ladmv/services/inquiries/VehicleInquiry";
 	private static final String ELT_NAMESPACE_URI = "http://rts.dt.com/ladmv/services/inquiries/ElectronicLienInquiry";
+	private static final String AR74_NAMESPACE_URI = "http://rts.dt.com/ladmv/services/inquiries/DailyMoniesSummaryInquiry";
 	private static final String TITLEREG_NAMESPACE_URI = "http://rts.dt.com/ladmv/services/transaction/TitleRegTransaction";
-	
-	
-	//Constants
 	public static final String VehicleInquiryType = "VM";
 
 	@Autowired
-	private ApplicationContext  ctx;
+	private ApplicationContext  appContext;
 
 	@PayloadRoot(localPart = "VehicleInquiryRequest", namespace = VM_NAMESPACE_URI)
 	@ResponsePayload
 	public VehicleInquiryResponse getVin(@RequestPayload VehicleInquiryRequest request) {
-		VehicleInquiryResponse response = ctx.getBean(VehicleInquiryResponse.class);
-		System.out.println("VehicleResponse Starts");
-		VehicleInquiryRepository vehicleInqRepository = ctx.getBean(VehicleInquiryRepository.class);
+		VehicleInquiryResponse vehicleInquiryResponse = appContext.getBean(VehicleInquiryResponse.class);
+		VehicleInquiryRepository vehicleInquiryRepository = appContext.getBean(VehicleInquiryRepository.class);
 		if(VehicleInquiryType.equalsIgnoreCase(request.getInquiry().getInquiryType())){
-			response.setInquiry(vehicleInqRepository.findVMInquirResponse(request.getInquiry().getVin()));
+			vehicleInquiryResponse.setInquiry(vehicleInquiryRepository.findVMInquirResponse(request.getInquiry().getVin()));
 		} else {
-			response.setInquiry(vehicleInqRepository.findLMInquirResponse(request.getInquiry().getPlateNum(), request.getInquiry().getRegExpYear().toString()));
+			vehicleInquiryResponse.setInquiry(vehicleInquiryRepository.findLMInquirResponse(request.getInquiry().getPlateNum(), request.getInquiry().getRegExpYear().toString()));
 		}
-		return response;
+		return vehicleInquiryResponse;
 	}
 	
 	/*
@@ -58,10 +58,21 @@ public class ServiceEndPoint {
 	@PayloadRoot(localPart = "TitleRegRequest", namespace = TITLEREG_NAMESPACE_URI)
 	@ResponsePayload
 	public TitleRegResponse getVin(@RequestPayload TitleRegRequest request) {
-		TitleRegResponse response = ctx.getBean(TitleRegResponse.class);
+		TitleRegResponse response = appContext.getBean(TitleRegResponse.class);
 		System.out.println("TitleRegResponse Starts");
-		TitleRegRepository titleRegRepository = ctx.getBean(TitleRegRepository.class);
+		TitleRegRepository titleRegRepository = appContext.getBean(TitleRegRepository.class);
+		
 //		response.setInquiry(titleRegRepository.receiveTitleReg(request.getTransaction().getVehicle().getVin());
 		return response;
 	}
+	
+	@PayloadRoot(localPart = "DailyMoniesSummaryInquiryRequest", namespace = AR74_NAMESPACE_URI)
+	@ResponsePayload
+	public DailyMoniesSummaryInquiryResponse getDailyMonies(@RequestPayload DailyMoniesSummaryInquiryRequest request) {
+		DailyMoniesSummaryInquiryResponse response = appContext.getBean(DailyMoniesSummaryInquiryResponse.class);
+		DailyMoniesInquiryRepository dailyMoniesInquiryRepository = appContext.getBean(DailyMoniesInquiryRepository.class);
+		return response;
+		
+	}
+	
 }
---
+++ src/main/java/com/dt/rts/ladmv/repository/DailyMoniesInquiryRepository.java
@@ -0,0 +1,14 @@
+package com.dt.rts.ladmv.repository;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DailyMoniesInquiryRepository {
+
+	@PostConstruct
+	public void initData() {
+		
+	}
+}
